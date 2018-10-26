0dbc36fdf6c283bfc29f0d89b7632e9a61c8e9b8
Addison Verger <addison.j.verger@gmail.com>
2018-10-26 12:31:21 -0500

    renderStudents complete

--- renderStudents.js
+++ renderStudents.js
@@ -1,10 +1,32 @@
+function individualStudent(studentInfo) {
+  return `<div style="width: 150px; height: 60px; border: 1px solid gray;
+          margin-bottom: 10px; text-align: center; ${presentColor(studentInfo.isPresent)}">
+            <div style="font-size: 140%">${studentInfo.name}</div>
+            <div><strong>${present(studentInfo.isPresent)}</strong></div>
+          </div>`
+}
 
 function renderStudents(students) {
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
-        </div>
-    `
+    var heading = `<h1>Roll Call!</h1>`
+    return heading + students.map(individualStudent).join('');
+}
+
+function presentColor(studentPresent) {
+  if (studentPresent === true) {
+    return `background-color: #abe519`
+  }
+  else {
+    return `background-color: #f24b46`
+  }
+}
+
+function present(studentPresent) {
+  if (studentPresent === true) {
+    return `Present`
+  }
+  else {
+    return `Absent`
+  }
 }
 
 function students() {
